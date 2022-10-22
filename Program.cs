2bd3a2001a6dce4b3ee9dff16f85e16f6c5dea47
KuzminaLiza <116429727+KuzminaLiza@users.noreply.github.com>
2022-10-22 12:17:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КОНОВАЛОВ ИЛЬЯ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            System.Console.WriteLine("Привет, Яндекс!");
+            name = Console.ReadLine();
+            Console.WriteLine("Приятно познакомиться. " + name + "!");
+
+
+        }
+    }
+}
